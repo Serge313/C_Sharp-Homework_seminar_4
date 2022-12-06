77aabbde401400ae15cb1e198866d6ded66491be
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-12-07 02:28:19 +0300

    Exercise_2 completed

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,40 @@
+﻿int yourNumber = GetNumber("Enter your number ");
+int sumOfDigits = GetSumOfDigits(yourNumber);
+Console.WriteLine($"{yourNumber} -> {sumOfDigits}");
+
+int GetSumOfDigits(int a)
+{
+    int c = 0;
+    bool isGreaterThenZero = a > 0;
+    if (!isGreaterThenZero)
+    {
+        Console.WriteLine("Your number should be > 0!");
+        return -1;
+    }
+    else
+    {
+        while (a > 0)
+        {
+            int cutDigit = a % 10;
+            c += cutDigit;
+            a /= 10;
+        }
+    }
+    return c;
+}
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+    if (!isParsed)
+    {
+        Console.WriteLine("Invalid value entered!");
+        return -1;
+    }
+    else
+    {
+        return number;
+    }
+}
